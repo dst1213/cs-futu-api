026c45807813a3c3d14922bc53feb2969d0eeada
Jason <neulf@hotmail.com>
2019-04-15 00:23:57 +0800

    dev

---
+++ cs-futu-api/common/NetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_futu_api.common
+{
+    public class NetManager
+    {
+        static private TcpClient netMgr = new TcpClient();
+        static private readonly NetManager netManager = new NetManager();
+
+        public void Connect()
+        {
+
+        }
+    }
+}
